51e94105c06133a8fabb4a5c677a2fc2411bacf3
Ben <quillaja@hotmail.com>
2024-05-10 00:56:10 -0700

    modify thread and process work function from closure to partial so it can be pickled

--- src/ela/slice.py
+++ src/ela/slice.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import multiprocessing
 import os
 from dataclasses import dataclass
+from functools import partial
 from itertools import repeat
 from typing import Callable, Iterable
 
@@ -21,6 +22,17 @@ class Slice:
     bottom: float
     area: float = 0
     weight: float = 0
+
+
+def _partial_slices(min_elev: float, max_elev: float, interval: float) -> list[Slice]:
+    slices: list[Slice] = []
+
+    elev = min_elev+interval
+    while elev <= max_elev + interval:
+        slices.append(Slice(top=elev, mid=elev-(interval/2), bottom=elev-interval))
+        elev += interval
+
+    return slices
 
 
 def _determine_worker_count() -> int:
@@ -45,39 +57,30 @@ def create_slices(surface: str, interval: float, be: Geoprocessor) -> list[Slice
     """
     min_elev, max_elev = be.min_max_elevations(surface)
     total_area = be.surface_area(surface, max_elev)
-    elev = min_elev+interval
+    slices = _partial_slices(min_elev, max_elev, interval)
+
     prev_area = 0
-    slices: list[Slice] = []
-    while elev <= max_elev + interval:
-        area = be.surface_area(surface, elev)
+    for s in slices:
+        area = be.surface_area(surface, s.top)
         slice_area = area-prev_area
-        slices.append(Slice(
-            top=elev,
-            mid=elev-(interval/2),
-            bottom=elev-interval,
-            area=slice_area,
-            weight=slice_area/total_area))
-        elev += interval
+        s.area = slice_area
+        s.weight = slice_area/total_area
         prev_area = area
 
     return slices
 
 
-def _slice_surface_area(surface: str, total_area: float, be: Geoprocessor) -> Callable[[Slice], Slice]:
+def _slice_surface_area(s: Slice, surface: str, total_area: float, be: Geoprocessor) -> Slice:
     """
-    Return a function to calculate the surface area of the Slice, which has values for
+    Calculate the surface area of the Slice, which has values for
     top, mid, and bottom, at a minimum. Returns a new complete Slice.
     """
-
-    def work(s: Slice) -> Slice:
-        area_from_top = be.surface_area(surface, s.top)
-        area_from_bottom = be.surface_area(surface, s.bottom)
-        slice_area = area_from_top-area_from_bottom
-        s.area = slice_area
-        s.weight = slice_area/total_area
-        return s
-
-    return work
+    area_from_top = be.surface_area(surface, s.top)
+    area_from_bottom = be.surface_area(surface, s.bottom)
+    slice_area = area_from_top-area_from_bottom
+    s.area = slice_area
+    s.weight = slice_area/total_area
+    return s
 
 
 def create_slices_threadpool(surface: str, interval: float, be: Geoprocessor) -> list[Slice]:
@@ -92,19 +95,13 @@ def create_slices_threadpool(surface: str, interval: float, be: Geoprocessor) ->
     """
     min_elev, max_elev = be.min_max_elevations(surface)
     total_area = be.surface_area(surface, max_elev)
-    slices: list[Slice] = []
-
-    elev = min_elev+interval
-    while elev <= max_elev + interval:
-        slices.append(Slice(top=elev, mid=elev-(interval/2), bottom=elev-interval))
-        elev += interval
+    incomplete_slices = _partial_slices(min_elev, max_elev, interval)
 
     WORKERS = _determine_worker_count()
+    work = partial(_slice_surface_area, surface=surface, total_area=total_area, be=be)
     # print(f"using {WORKERS} threads")
     with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as pool:
-        complete_slices = pool.map(
-            _slice_surface_area(surface, total_area, be),
-            slices, chunksize=4)
+        complete_slices = pool.map(work, incomplete_slices, chunksize=4)
 
     complete_slices = sorted(complete_slices, key=lambda s: s.bottom)
     return complete_slices
@@ -122,19 +119,13 @@ def create_slices_processpool(surface: str, interval: float, be: Geoprocessor) -
     """
     min_elev, max_elev = be.min_max_elevations(surface)
     total_area = be.surface_area(surface, max_elev)
-    slices: list[Slice] = []
-
-    elev = min_elev+interval
-    while elev <= max_elev + interval:
-        slices.append(Slice(top=elev, mid=elev-(interval/2), bottom=elev-interval))
-        elev += interval
+    incomplete_slices = _partial_slices(min_elev, max_elev, interval)
 
     WORKERS = _determine_worker_count()
+    work = partial(_slice_surface_area, surface=surface, total_area=total_area, be=be)
     # print(f"using {WORKERS} threads")
     with multiprocessing.Pool(processes=WORKERS) as pool:
-        complete_slices = pool.map(
-            _slice_surface_area(surface, total_area, be),
-            slices, chunksize=4)
+        complete_slices = pool.map(work, incomplete_slices, chunksize=4)
 
     complete_slices = sorted(complete_slices, key=lambda s: s.bottom)
     return complete_slices
