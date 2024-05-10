a4801c10fd41056aa387c4461921e6d427ead665
Ben <quillaja@hotmail.com>
2024-05-10 12:41:40 -0700

    package comment

--- src/ela/__init__.py
+++ src/ela/__init__.py
@@ -1,5 +1,10 @@
-import typing
+"""
+The ela module provides classes and data structures for calculating the
+Equilibrium Line Altitude (ELA) for glaciers based on a DEM of their surface.
 
+`Geoprocessor` should be implemented by packages that wish to interface with
+software like ArcGIS Pro or QGIS to provide the core geospatial processing.
+"""
 from .geoprocessing import Geoprocessor, HistData
 from .methods import (AABR, AABR2D, AAR, AAR2D, ELA, AABROriginal, AAROriginal,
                       ELAMethod)
